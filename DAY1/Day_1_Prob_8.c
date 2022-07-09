50fe5a96fd0876fb433a68f351a79f9146563773
Pallavi-69251 <108972818+Pallavi-69251@users.noreply.github.com>
2022-07-09 14:33:36 +0530

    Create Day_1_Prob_8.c

---
+++ DAY1/Day_1_Prob_8.c
@@ -0,0 +1,10 @@
+8)Consider a character input and find its ASCII value. 
+
+#include <stdio.h>
+int main()
+{
+   char a;
+   printf("enter the charecter\n");            
+   scanf("%c",&a);                       //Read the charector from user
+   printf("the ASCII value of charecter is %d",a);          //finding ASCII value of charector
+}
