41431fc71ea8b58600e801f933ab7325e8c228b7
Pallavi-69251 <108972818+Pallavi-69251@users.noreply.github.com>
2022-07-09 14:14:35 +0530

    Create Day_1_Prob_4.c

---
+++ DAY1/Day_1_Prob_4.c
@@ -0,0 +1,16 @@
+4)Take a 6-digit number as input from the user and reverse the number. 
+Make sure that the number does not include a ‘0’ in any of its digits.
+
+#include <stdio.h>
+
+int main()
+{
+    int x,r=0;
+    printf("enter 6 digits\n"); 
+    scanf("%d",&x);           //  Read the 6-digit number from user
+    while(x>0){
+    r=r*10+x%10;               // By using this we are shifting ten's place to hundread's place
+    x=x/10;                     // By dividing number it remove 1 digit 
+    }
+    printf("the reversing number is %d",r);           //finally it will get reversed number
+}
