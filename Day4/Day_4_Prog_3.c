d23a89d3ff28e26452f9f22399a82351978abed9
Pallavi-69251 <108972818+Pallavi-69251@users.noreply.github.com>
2022-07-10 13:06:48 +0530

    Update Day_4_Prog_3.c

--- Day4/Day_4_Prog_3.c
+++ Day4/Day_4_Prog_3.c
@@ -7,5 +7,5 @@ int main()
   printf("enter the value of a\n");           
   printf("enter the value of b\n");
    scanf("%d %d",&a,&b);             //Read the two variables from user
-    a>b?printf("a is greater"):printf("b is greater");   //conditional operator find which is greater
+    a>b?printf("a is greater"):printf("b is greater");   //by using conditional operator to find which is greater
 }
