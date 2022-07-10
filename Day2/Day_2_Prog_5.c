6a7a9831a0f6decdd3454740f0d2a2addb849118
Pallavi-69251 <108972818+Pallavi-69251@users.noreply.github.com>
2022-07-10 07:54:45 +0530

    Create Day_2_Prog_5.c

---
+++ Day2/Day_2_Prog_5.c
@@ -0,0 +1,10 @@
+5))A message is encoded in the form of numbers and sent to you.
+ Write a program to decode the message. (HINT: A=1, B=2, … so on) 
+*******************************************************************************/
+#include <stdio.h>
+int main()
+{
+int n=2,r;            //initialize variable
+   r=64+n;              //by using this 65 is A so taken as 64  
+     printf("%c",r);      //so 64 and variable prints the decoded charecetor
+ }
