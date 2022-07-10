01ac7f62b2fc11eeed1d2384dbc406e8281de42b
Pallavi-69251 <108972818+Pallavi-69251@users.noreply.github.com>
2022-07-10 11:31:28 +0530

    Create Day_4_Prog_3.c

---
+++ Day4/Day_4_Prog_3.c
@@ -0,0 +1,11 @@
+1)BIGGEST OF TWO NUMBERS USING CONDITIONAL OPERATOR
+
+#include <stdio.h>
+int main()
+{
+   int a,b;
+  printf("enter the value of a\n");           
+  printf("enter the value of b\n");
+   scanf("%d %d",&a,&b);             //Read the two variables from user
+    a>b?printf("a is greater"):printf("b is greater");   //conditional operator find which is greater
+}
